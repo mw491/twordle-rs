4a215ec330ae44b91d764f798b6ebc9f06f6e537
Musa W <musaiw@outlook.com>
2026-06-02 16:05:39 +0100

    use musl build of trunk

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -e
 export PATH="$HOME/.cargo/bin:$PATH"
 rustup target add wasm32-unknown-unknown
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
-cargo binstall --no-confirm trunk
+cargo binstall --no-confirm --targets x86_64-unknown-linux-musl trunk
 trunk build --release --public-url=/
